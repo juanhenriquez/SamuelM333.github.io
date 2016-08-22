e937c6e33164eb85b1e1c727b1f82dd4000416e1
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-08-21 21:40:22 -0700

    Add smooth-scroll plugin locally

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,21 @@
-window.onload = function(){
-    $(".button-collapse").sideNav();
-    smoothScroll.init({
-        easing: 'Linear',
-        updateURL: false,
+// window.onload = function(){
+//     $(".button-collapse").sideNav();
+//     smoothScroll.init({
+//         easing: 'Linear',
+//         updateURL: false,
+//     });
+// };
+
+;(function(w, $){
+
+    $(document).ready(function(){
+
+        $(".button-collapse").sideNav();
+        smoothScroll.init({
+            easing: 'easeOutQuad',
+            updateURL: false,
+        });
+
     });
-};
+
+})(this, jQuery);
